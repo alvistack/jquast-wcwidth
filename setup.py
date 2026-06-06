3b0846609545ee5f4e8cd07b323343a3e4d44434
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-06-06 15:42:04 +0800

    alvistack/0.8.0
        git clean -xdf
        git submodule sync --recursive
        git submodule update --recursive --init
        git submodule foreach --recursive git checkout -- .
        git submodule foreach --recursive git clean -xdf
        tar zcvf ../python-wcwidth_0.8.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-wcwidth.spec ../python-wcwidth_0.8.0-1.spec
        cp ../python*-wcwidth*0.8.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/jquast-wcwidth-0.8.0/
        rm -rf ../python*-wcwidth*0.8.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='wcwidth',
+    version='0.8.0',
+    description='Measures the displayed width of unicode strings in a terminal',
+    author_email='Jeff Quast <contact@jeffquast.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: 3.15',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Localization',
+        'Topic :: Terminals',
+        'Typing :: Typed',
+    ],
+    packages=[
+        'wcwidth',
+        'wcwidth.table_grapheme_overrides',
+    ],
+)
